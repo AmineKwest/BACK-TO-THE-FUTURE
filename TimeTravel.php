8cd3992e644ffae38fa3df41938b73e6c0c90692
Amine Kwest <bakhti.amine.90@gmail.com>
2018-05-31 15:46:22 +0200

    modif

--- TimeTravel.php
+++ TimeTravel.php
@@ -4,6 +4,7 @@ class TimeTravel
   public $start;
   public $end;
   public $interval;
+
   public function __construct($start, $end){
     $this->start = new DateTime($start);
     $this->end = new DateTime($end);
@@ -19,27 +20,11 @@ class TimeTravel
     return ($this->start->sub($this->interval)->format('d/m/Y h:i:s'));
   }
   public function backToFutureStepByStep($step){
-    $i=1;
     $step = new DateInterval($step);
     $stop = new DatePeriod($this->start, $step, $this->end);
     foreach ($stop as $date){
-      ?><div class="col-xs-2 col-md-2 ">
-        <table>
-          <thead>
-            <tr>
-              <th colspan="2">Date number <?php echo $i++;?></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td colspan="2"><?php echo $date->format('M d Y A g:i');?></td>
-            </tr>
-          </tbody>
-        </table>
-      </br>
-    </div>
-    <?php
-  }
+    echo $date->format('M ' .'d ' .'Y ' .'A ' .'g:' .'i') .'<br>';
+}
 }
 }
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include "class.php";
+include "TimeTravel.php";
 ?>
 <html>
 <head>
@@ -7,34 +7,44 @@ include "class.php";
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
   <link rel="stylesheet" href="style.css">
   <title>BACK TO THE FUTURE</title>
 </head>
 <body>
   <p>
 
-    <?php $Doc = new TimeTravel("31-12-1985", "");?>
+    <?php $doc = new TimeTravel("31-12-1985", "");?>
     Suite à une expérience ratée avec sa nouvelle machine,
     le 31 décembre 1985, Doc s'est retrouvé projeté dans le temps.
     D'après les notes qu'il a laissées, Marty en déduit qu'il a été projeté
-    le <?php $Doc->findDate("PT1000000000S")?>.
+    le <?php
+    echo $doc->findDate("PT1000000000S")?>.
 
-    </br>
+  </br>
 
-    <?php $Marty= new TimeTravel("23-04-1954 10:35:20", "31-12-1985 06:35:18");
-    $date1= "23-04-1954 06:35";
-    $date2= "31-12-1985 06:35";
-    echo $Marty->getTravelInfo($date1,$date2)?> qui sépare Marty et Doc.
-    </br>Marty a retrouvé Doc mal en point.
-    Il faut le ramener au plus vite à notre époque pour le soigner.
-    Mais le réservoir de la Dolorean est endommagé.
-    Après une rapide expertise, Doc estimes que
-    la voiture ne peut plus faire des bonds dans le temps que, au mieux,
-    d’1 mois plus 1 semaine et 1 jour.
-    Il faudra refaire le plein de combustible à chaqu'une des date suivantes:
-    <?php $Marty->backToFutureStepByStep("P1M8D");?>
-  </p>
-
+  <?php $marty= new TimeTravel("23-04-1954 10:35:20", "31-12-1985 06:35:18");
+  echo $marty->getTravelInfo()?> qui sépare Marty et Doc.
+</br>
+Marty a retrouvé Doc mal en point.
+Il faut le ramener au plus vite à notre époque pour le soigner.
+Mais le réservoir de la Dolorean est endommagé.
+Après une rapide expertise, Doc estimes que
+la voiture ne peut plus faire des bonds dans le temps que, au mieux,
+d’1 mois plus 1 semaine et 1 jour.
+Il faudra refaire le plein de combustible à chaqu'une des date suivantes:
+        <table>
+          <thead>
+            <tr>
+              <th colspan="2">All Date </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td colspan="2"><?php
+              echo $marty->backToFutureStepByStep("P1M8D");?></td>
+            </tr>
+          </tbody>
+        </table>
+      </br>
 </body>
 </html>
